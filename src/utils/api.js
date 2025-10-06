fccd92e79bafc793e620d299d52cefeb9818ffe2
Raghul <rraghul2k3@gmail.com>
2025-10-06 15:38:23 +0530

    newapi

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_PORT, 
+  baseURL: import.meta.env.VITE_PORT+"/api", 
 });
 
 api.interceptors.request.use((config) => {
